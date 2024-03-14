6146db4d816efedeef3e8a28a49d8b1f3fdcd7a6
celyssa <143862783+celyssa@users.noreply.github.com>
2024-03-14 08:30:24 +0000

    first commit

---
+++ ERC20/scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const lock = await ethers.deployContract("TTT", ["0x00808eB50E96614417C74F1a34007c740c987594"]); 
+
+  await lock.waitForDeployment();
+
+  console.log(
+    `Token deployed to ${lock.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
